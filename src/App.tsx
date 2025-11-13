933a8f0a8ba12945d2fbc3edfa8bc8654a22ea57
bastian <bastian.peire@gmail.com>
2025-11-13 13:35:25 +0100

    fix: fix errors in index.html file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>A Ceph sizer htmlFor capacity, configuration, and subscriptions</h1>
+      <table> 
+        <thead>
+          <tr>
+            <th scope="col"> </th>
+            <td></td>
+            <td scope="col">
+              (ignored in calculation - input only)TiB
+            </td>
+            <td scope="col">(used htmlFor calculation)TB</td>
+          </tr>
+        </thead>
+        <tbody>
+            <form id="general-values"> 
+                <tr>
+                    <td>Converter from TiB into TB: (value not applied - calulation only; only TB used in calc)</td>
+                    <td><button id="button-capacity-tib" type="submit" data-target="tib">Convert</button></td>
+                    <td width="250" align="center">
+                        <input type="text" id="global-capacity-tib-input" />
+                    </td>
+                    <td width="250" align="center">
+                        <p className="converted-capacity" id="tib"><span className="value">0</span></p>
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <th scope="row">All servers have the same NIC, cores & mem in config ?</th>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-similar-config" name="global-similar-config" value="1" checked />
+                        <label htmlFor="similar-config">Same config</label>
+                    </td>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-dissimilar-config" name="global-similar-config" value="0" />
+                        <label htmlFor="similar-config">Different config</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">All servers have the same number and types of media htmlFor capacity in config ?</th>
+                    
+                    <td align="center">
+                        <input type="radio" id="radiobutton-similar-media-config" name="global-similar-media-config" value="1" checked />
+                        <label htmlFor="similar-media-config">Same config</label>
+                    </td>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-dissimilar-media-config" name="global-similar-media-config" value="0" />
+                        <label htmlFor="similar-media-config">Similar config</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">All servers have the exactly the same number and types of media in config ?</th>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-same-media-config" name="global-same-media-config" value="1" checked />
+                        <label htmlFor="same-media-config">Exactly same config</label>
+                    </td>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-different-media-config" name="global-same-media-config" value="0" />
+                        <label htmlFor="same-media-config">Media config apply to slots available</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">HDDs use separate servers ?</th>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-separate-hdd-config" name="global-separate-hdd-config" value="1" checked />
+                        <label htmlFor="separate-hdd-config">Separate servers htmlFor HDD</label>
+                    </td>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-nonseparate-media-config" name="global-separate-hdd-config" value="0" />
+                        <label htmlFor="separate-hdd-config">Mix HDD and flash on servers</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">SSD with dedicated but combined RocksDB/WAL use separate servers ?</th>
+                    
+                    <td align="center">
+                        <input type="radio" id="radiobutton-separate-nvmetype5-config" name="global-separate-nvmetype5-config" value="1" checked />
+                        <label htmlFor="separate-nvmetype5-config">Use separate servers</label>
+                    </td>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-nonseparate-nvmetype5-config" name="global-separate-nvmetype5-config" value="0" />
+                        <label htmlFor="separate-nvmetype5-config">Use same servers</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">SSD with dedicated RocksDB/WAL and dedicated WAL or NVMe with dedicated WAL use separate servers ?</th>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-separate-nvmetype3-config" name="global-separate-nvmetype3-config" value="1" checked />
+                        <label htmlFor="separate-nvmetype3-config">Use separate servers</label>
+                    </td>
+                    <td align="center">
+                        <input type="radio" id="radiobutton-nonseparate-nvmetype3-config" name="global-separate-nvmetype3-config" value="0" />
+                        <label htmlFor="separate-nvmetype3-config">Use same servers</label>
+                    </td>
+                </tr>
+            </form>
+        </tbody>
+    </table>
+    <table>
+        <tbody>
+            <tr>
+                <td>Chassis config file load:</td>
+                <td>
+                    <input type="file" id="input-filename-chassis-config" />
+                </td>
+                <td>Workload config file load:</td>
+                <td>
+                    <input type="file" id="input-filename-workload-config" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table>
+        <tbody>
+            <tr>
+                <td style={{minWidth: "50px"}}>
+                    <td><b>Error messages:</b></td>
+                    <td style={{minWidth: "100px", border: "1px solid black"}}>
+                        <p id="error-message" />
+                    </td>
+                </td>
+                <td style={{minWidth: "50px", textAlign: "left"}}>
+                    <td><b>InhtmlFormation & Warnings:</b></td>
+                    <td style={{minWidth: "100px", border: "1px solid black"}}>
+                    <p id="misc-message"></p>   
+                </td>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
   )
 }
 
-export default App
+export default App;
